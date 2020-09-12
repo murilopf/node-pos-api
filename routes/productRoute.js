f9a42a3b9241b5b6784f5d208203ba15a1ad079c
Murilo <mux.pfernandes@gmail.com>
2020-09-12 16:13:24 -0300

    feature- novos metodos rest

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -30,4 +30,83 @@ router.get('/', (req, res) => {
   })
 });
 
+router.get('/:productId', (req, res) => {
+  const id = req.params.productId;
+  Produto.findById(id, (error, product) => {
+    if(error){
+      res.status(500).json({
+        message: "Erro ao tentar encontrar produto; ID inválido"
+      })
+    }else if(product == null){
+      res.status(400).json({
+        message: "Nenhum produto encontrado com o ID informado"
+      })
+    }else{
+      res.status(200).json({
+        message: "request success",
+        product: product
+      })
+    }
+  })
+});
+
+router.put('/:productId', (req, res) => {
+  const id = req.params.productId;
+  Produto.findById(id, (error, product) => {
+    if(error){
+      res.status(500).json({
+        message: "Erro ao tentar encontrar produto; ID inválido"
+      })
+    }else if(product == null){
+      res.status(400).json({
+        message: "Nenhum produto encontrado com o ID informado"
+      })
+    }else{
+
+      if(req.body.name)
+        product.name = req.body.name;
+
+      if(req.body.price)
+        product.price = req.body.price;
+
+      if(req.body.description)
+        product.description = req.body.description;
+
+      product.save((error)=> {
+        if(error)
+          res.send("Erro ao atualizar o produto ", error);
+    
+        res.status(200).json({
+          message: 'produto atualizado com sucesso',
+          productUpdated: product
+        });
+      })
+    }
+  })
+});
+
+router.delete('/:productId', (req, res) => {
+  const id = req.params.productId;
+  Produto.findById(id, (error, product) => {
+    if(error){
+      res.status(500).json({
+        message: "Erro ao tentar encontrar produto; ID inválido"
+      })
+    }else if(product == null){
+      res.status(400).json({
+        message: "Nenhum produto encontrado com o ID informado"
+      })
+    }else{
+      product.delete((error)=> {
+        if(error)
+          res.send("Erro ao deletar o produto ", error);
+    
+        res.status(200).json({
+          message: 'produto excluido com sucesso'
+        });
+      })
+    }
+  })
+});
+
 module.exports = router;
